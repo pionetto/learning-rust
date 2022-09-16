53e351beffaf5cb5e07c38c6eababd8dd8e3ce42
Pio Lima <pio.lima@gmail.com>
2022-09-16 07:59:13 -0400

    First Rust Language exercises

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    //println!("Hello, world Pio!");
+    let x: u32 = 5;
+    //i8, u8, i16, u16, i23, u32, i64, u64, isize, usize
+    //f32, f64
+
+    let a = 1 + 20;
+
+    let s = 30 - 20;
+    let m = 5 * 20;
+    let d = 4/6;
+    let r = 49 % 2;
+
+    //bool: true/false
+    let c = 'z';
+
+    let t: (i32, f64, char) = (42, 6.12, 'j');
+    let (_, _, x) = t;
+    let a = [1, 2, 3, 4, 5, 6, 7, 8];
+    let a1 = a[0];
+    
+
+}
